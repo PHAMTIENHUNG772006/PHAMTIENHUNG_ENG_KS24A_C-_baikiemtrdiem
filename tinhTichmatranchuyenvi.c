dc96cd7bd6a545b004a8f855d149bb522399c172
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-20 16:16:43 +0700

    Add files via upload

--- tinhTichmatranchuyenvi.c
+++ tinhTichmatranchuyenvi.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int t;  // So bo test
+    int t;  
     scanf("%d", &t);
-    
     for (int k = 1; k <= t; k++) {
         int n, m;
         scanf("%d %d", &n, &m);
-        
-        int a[n][m];  // Ma tran A
-        // Doc ma tran A
+        int a[n][m];  
+        //doc ma tran a
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 scanf("%d", &a[i][j]);
             }
         }
         
-        // Tinh ma tran tich A * A^T, ma tran ket qua co kich thuoc n x n
+        //tinh ma tran tich a * a^T va ma tran ket qua co kich thuoc n x m
         int result[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 result[i][j] = 0;
-                // Tinh tich vo huong cua hang i cua A va hang j cua A^T (hay cot j cua A)
                 for (int k = 0; k < m; k++) {
                     result[i][j] += a[i][k] * a[j][k];
                 }
             }
         }
         
-        // In ket qua
+        //hienthiketqua
         printf("Test %d:\n", k);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
